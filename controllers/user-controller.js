41f09a687a08dbe4d9eb203816401f9df77297f1
Stirish <floo.swartzy@hotmail.fr>
2023-02-10 15:06:27 +0100

    GetAllUsers sans le MDP ni Email

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,7 @@
 const UserModel = require('../models/user-model');
+const ObjectID = require('mongoose').Types.ObjectId;
 
+// Create User
 module.exports.signUp = async (req, res) => {
     const {pseudo, email, password} = req.body;
 
@@ -8,6 +10,12 @@ module.exports.signUp = async (req, res) => {
         res.status(201).json({user: user._id});
     }
     catch(err) {
-        res.status(400).send({err})
+        res.status(400).send({err});
     }
-}
+};
+
+// Read All Users
+module.exports.getAllUsers = async (req, res) => {
+    const users = await UserModel.find().select(['-password', '-email']);
+    res.status(200).json(users);
+};
--- routes/user-route.js
+++ routes/user-route.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const authController = require ('../controllers/user-controller')
+const userController = require ('../controllers/user-controller')
 
-router.post('/register', authController.signUp);
+
+router.post('/register', userController.signUp);
+router.get('/', userController.getAllUsers);
 
 module.exports = router;
