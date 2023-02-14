73445959d0ec70944e7c55c496fd7118650996df
Stirish <floo.swartzy@hotmail.fr>
2023-02-14 20:01:43 +0100

    Gestion des erreurs

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,5 +1,6 @@
 const UserModel = require('../models/user-model');
 const jwt = require('jsonwebtoken');
+const { signUpErrors, signInErrors } = require('../utils/error-util');
 const maxAge = 3 * 24 * 60 * 60 * 1000;
 
 const createToken = (id) => {
@@ -16,7 +17,8 @@ module.exports.signUp = async (req, res) => {
         res.status(201).json({user: user._id});
     }
     catch(err) {
-        res.status(400).send({err});
+        const errors = signUpErrors(err);
+        res.status(400).send({errors});
     }
 };
 
@@ -26,11 +28,12 @@ module.exports.signIn = async (req, res) => {
     try {
         const user = await UserModel.login(email, password);
         const token = createToken(user._id);
-        res.cookie('jwt', token, { httpOnly: true, maxAge });
-        res.status(200).json({ user: user._id});
+        res.cookie('jwt', token, {httpOnly: true, maxAge});
+        res.status(200).json({user: user._id});
     }
     catch (err) {
-        res.status(400).json(err);
+        const errors = signInErrors(err);
+        res.status(400).send({errors});
     }
 };
 
