200c4a4dc83e93adca761b0419dd231e432a5d0f
Stirish <floo.swartzy@hotmail.fr>
2023-02-15 03:00:17 +0100

    Config pour Photo de profil

--- routes/user-route.js
+++ routes/user-route.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth-controller');
 const userController = require('../controllers/user-controller');
+const uploadController = require('../controllers/upload-controller');
+const multer = require('multer');
+const upload = multer();
 
 // User Authentication
 router.post('/register', authController.signUp);
@@ -12,5 +15,7 @@ router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getOneUser);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
+
+router.post('/upload', upload.single('file'), uploadController.uploadProfil);
 
 module.exports = router;
